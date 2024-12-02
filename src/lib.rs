31a89adaa4c54204c9a5556de75b864a13a22696
yuanan <yuanan@htgd.com.cn>
2024-12-02 09:17:40 +0800

    【框架搭建】update

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
+use std::error::Error;
+use serde::{Deserialize, Serialize};
+use crate::database::{make_db_pool, DB_POOL};
+
 pub mod database;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MyBlogConfig {
+    pub application: Application,
+    pub database: Database,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Database {
+    pub url: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Application {
+    pub host: String,
+    pub port: usize,
+}
+
+pub async fn init_config() -> Result<MyBlogConfig, Box<dyn Error>> {
+    let config: MyBlogConfig = serde_yml::from_str(include_str!("D:/code/mblog/mblog.yaml"))?;
+    let pool = make_db_pool(config.database.url.as_str()).await?;
+    DB_POOL.set(pool).unwrap();
+    Ok(config)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
-use mblog::database::{db_pool, make_db_pool, DB_POOL};
 use salvo::prelude::*;
-use serde::{Deserialize, Serialize};
 use tracing::log::{log, Level};
+use mblog::{init_config};
 
 #[handler]
 async fn hello() -> &'static str {
     "hello world"
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct MyBlogConfig {
-    database: Database,
-}
-#[derive(Debug, Serialize, Deserialize)]
-struct Database {
-    url: String,
-}
-
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
-    let router = Router::new().get(hello);
-    let acceptor = TcpListener::new("127.0.0.1:7878").bind().await;
-    let config: MyBlogConfig =
-        serde_yml::from_str(include_str!("/home/mikasa/code/mblog/mblog.yaml"))
-            .expect("failed to load config.yml");
-    let pool = make_db_pool(config.database.url.as_str()).await;
-    if let Ok(pool_impl) = pool {
-        DB_POOL.set(pool_impl).unwrap();
+    match init_config().await {
+        Ok(config) => {
+            let router = Router::new().get(hello);
+            let acceptor = TcpListener::new(format!("{}:{}", config.application.host, config.application.port)).bind().await;
+            Server::new(acceptor).serve(router).await;
+        }
+        Err(e) => {
+            log!(Level::Error,"{}",e.to_string())
+        }
     }
-    log!(Level::Info,"{:?}",db_pool().options());
-    Server::new(acceptor).serve(router).await;
 }
